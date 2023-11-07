db8551e6b71596d27dfca12b345fd5b7df755d9f
Peti0 <113418512+Peti0@users.noreply.github.com>
2023-11-07 10:49:00 +0100

    Add files via upload

---
+++ TvShop/layout/footer.php
@@ -0,0 +1,62 @@
+<style>
+     
+        footer {
+            background-color: #333;
+            color: #fff;
+            padding: 60px;     
+            bottom: 0;
+            width: 100%;
+           
+        }
+
+        .footer-container {
+            display: flex;
+            justify-content: space-between;
+            flex-wrap: wrap;
+        }
+
+        .footer-section {
+            width: 30%;
+        }
+
+        .footer-section h3 {
+            font-size: 20px;
+        }
+
+        .footer-section ul {
+            list-style: none;
+            padding: 0;
+        }
+
+        .footer-section li {
+            margin-bottom: 0px;
+        }
+    </style>
+<footer>
+        <div class="footer-container">
+            <div class="footer-section">
+                <h3>Kapcsolat</h3>
+                <p>Cím: Szent István Utca, Kecskemét, 6000</p>
+                <p>Telefon: +36 20 234 5678</p>
+                <p>E-mail: tvshop@gmail.com</p>
+            </div>
+
+            <div class="footer-section">
+                <h3>Hasznos linkek</h3>
+                <ul>
+                    <li><a href="index.php?menu=home">Kezdőlap</a></li>
+                    <li><a href="index.php?menu=rolunk">Rólunk</a></li>
+                    <li><a href="index.php?menu=regisztracio">Regisztráció</a></li>
+                    
+                </ul>
+            </div>
+
+            <div class="footer-section">
+                <h3>Elérhetőségek</h3>
+                <img src="./images/facebook.png" height="30" width="45" alt="facebook">
+                <img src="./images/Instagram.png" height="30" width="42" alt="instagram">
+            </div>
+        </div>
+
+        
+    </footer>
---
+++ TvShop/layout/header.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="tv.css"/>
+<div id="b" class="w3-container w3-teal" >
+   <h1><img src="./images/television.svg" alt="alt" height="70" /> Televízió vásárlás</h1>
+</div>
