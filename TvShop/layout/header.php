09a27a927c2bb54f50ae0c330b8c16381fbe48bf
Peti0 <113418512+Peti0@users.noreply.github.com>
2023-11-07 10:46:32 +0100

    Delete TvShop directory

--- TvShop/layout/header.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" href="tv.css"/>
-<div id="b" class="w3-container w3-teal" >
-    <center><h1><img src="./images/television.svg" alt="alt" height="70" /> Televízió vásárlás</h1></center>
-</div>
